7d39ad966051cfc1e373bec134e1a41254e65c1e
anjaliruth <ruthanjali0211@gmail.com>
2023-08-27 16:03:59 +0100

    cart shows on click, but needs to be superimposed

--- frontend/src/components/Cart/Cart.tsx
+++ frontend/src/components/Cart/Cart.tsx
@@ -1,30 +1,40 @@
-import React from "react";
+import { useState } from "react";
 
 export default function Cart({ cartItems }) {
+  const [isCartOpen, setIsCartOpen] = useState(false);
+  console.log("Cart Items:", cartItems);
+  console.log("isCartOpen:", isCartOpen);
+
   return (
     <div className="">
-      <button className="cartButton">
+      <button className="cartButton" onClick={() => setIsCartOpen(!isCartOpen)}>
         <span className="material-symbols-outlined">shopping_cart</span>
         <h3>{cartItems.length}</h3>
       </button>
 
-      <div className="cartList">
-        {cartItems.map((item) => (
-          <div key={item.id} className="cartItem">
-            <img src={item.image} alt={item.name} />
-            <p className="productNameInCart">{item.name}</p>
-            <h4> £{item.price}</h4>
-            <h3>{item.description}</h3>
+      {isCartOpen && (
+        <div className="cartList">
+          {cartItems.map((item) => (
+            <div key={item.id} className="cartItem">
+              <img src={item.image} alt={item.name} />
+              <p className="productNameInCart">{item.name}</p>
+              <h4> £{item.price}</h4>
+              <h3>{item.description}</h3>
+            </div>
+          ))}
+          <div className="totalPrice">
+            <h2>Total:</h2>
+            <h2>
+              {" "}
+              £
+              {cartItems.reduce(
+                (total, item) => total + parseFloat(item.price),
+                0
+              )}
+            </h2>
           </div>
-        ))}
-
-      </div>
-
-      <div className="totalPrice">
-        <h2>Total:</h2>
-        <h2> £{cartItems.reduce((total, item) => total + parseFloat(item.price), 0)}</h2>
-
-      </div>
+        </div>
+      )}
     </div>
   );
 }
