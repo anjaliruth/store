cdcf80158c59fa0157c20716c141e54bf58cfe1b
anjaliruth <ruthanjali0211@gmail.com>
2023-09-14 15:59:07 +0100

    category works

--- JSfrontend/src/components/SearchBar/SearchBar.jsx
+++ JSfrontend/src/components/SearchBar/SearchBar.jsx
@@ -10,8 +10,9 @@ export default function SearchBar({
   isCartOpen,
 }) {
   const [filter, setFilter] = useState("");
+  const [category, setCategory] = useState("");
   const filteredItems = dataFromServer.filter((item) => {
-    return item.name.toLowerCase().includes(filter.toLowerCase());
+    return (item.name.toLowerCase().includes(filter.toLowerCase()) && (category === "" || item.category === category));
   });
   console.log(filter);
   return (
@@ -21,6 +22,18 @@ export default function SearchBar({
         onChange={(e) => setFilter(e.target.value)}
         className="inputField"
       />
+      <select
+        onChange={(e) => setCategory(e.target.value)}
+        className="category"
+      >
+        <option value="">Category</option>
+        <option value="Hat">Hat</option>
+        <option value="Hoodie">Hoodie</option>
+        <option value="Jacket">Jacket</option>
+        <option value="Mask">Mask</option>
+        <option value="Sweater">Sweater</option>
+        <option value="T-Shirt">T-Shirt</option>
+      </select>
 
       <ProductGrid
         filteredItems={filteredItems}
